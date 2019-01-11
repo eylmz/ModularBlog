8eb3a0f0a19bbeed5f31bb58df556fcb0aadd6bd
Emre <eylmz058@gmail.com>
2019-01-11 22:34:35 +0300

    Soft Delete to Category

--- Modules/Category/Entities/Category.php
+++ Modules/Category/Entities/Category.php
@@ -4,9 +4,12 @@ namespace Modules\Category\Entities;
 
 use Modules\Blog\Entities\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ["name"];
 
     public function posts(){
